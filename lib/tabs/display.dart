0fad4ade19f8f7bb8fbb9f17c30d7bc6474e5ef7
ing.javierdavidjp@gmail.com <ing.javierdavidjp@gmail.com>
2023-02-08 19:29:08 -0500

    2nd_commit

--- lib/tabs/display.dart
+++ lib/tabs/display.dart
@@ -1,15 +1,14 @@
-import 'dart:html';
 
 import 'package:flutter/material.dart';
 
 class DisplayWidger extends StatelessWidget {
-  final Image;
-  DisplayWidger(this.Image);
+  String image;
+  DisplayWidger(this.image);
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Image.assets(img),
+        child: Image.asset(image)
       ),
     );
   }
